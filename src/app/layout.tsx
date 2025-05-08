20c46bc50c8b5b8fb0486044660c3d8fd1c41ff6
Adrian Zinko <zinko.adrian00@gmail.com>
2025-05-08 12:15:02 +0200

    feat: add page speed insight integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<SpeedInsights />
+				{children}
+			</body>
 		</html>
 	);
 }
